42139890b0f31547587d98e19e5ac9a9a3862bd9
Matt Shultz <teamshultz@gmail.com>
2018-05-21 20:24:34 -0500

    Organize initial variables and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,72 @@
-/*
- * Create a list that holds all of your cards
- */
-const cardList = [
-    'space-shuttle', 
-    'bath', 'bullhorn', 
-    'chess-knight', 
-    'dove', 
-    'futbol'
-];
-//Create a clone of the card list (for pairing) and push into original array, creating the final deck array
-const cardPairs = cardList;
-var deck = cardList.concat(cardPairs);
+var deck,
+    cardList,
+    openCards,
+    clickDisabled,
+    moveCount,
+    starCount,
+    stars,
+    movesLabel,
+    timer,
+    minutesLabel,
+    secondsLabel,
+    totalSeconds,
+    timerStarted,
+    starWinText,
+    possiblePairs,
+    currentPairs;
 
-//Set up starting variables
-var openCards = [];
-var clickDisabled = false;
-var moveCount = 0;
-var starCount = 3;
-var starWinText = "stars";
-var possiblePairs = cardList.length;
-var currentPairs = 0;
+//Add click listener to the deck and flip the card if it's clicked
+const deckContainer = document.querySelector('.deck');
+deckContainer.addEventListener('click', flipCard);
 
-var stars = document.getElementById('rating');
-var movesLabel = document.getElementById("moves");
+//Add click listener to the reset button
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', resetGame);
 
-/*
- * Game timer logic
- */
+function initializeValues() {
+    //Set up starting variables
+    openCards = [];
+    clickDisabled = false;
+    moveCount = 0;
+    starCount = 3;
+    starWinText = 'stars';
+    currentPairs = 0;
+    cardList = [
+        'space-shuttle',
+        'bath', 'bullhorn',
+        'chess-knight',
+        'dove',
+        'futbol'
+    ];
+    possiblePairs = cardList.length;
+    stars = document.querySelector('.rating');
+    movesLabel = document.querySelector('.moves');
+    minutesLabel = document.querySelector('.minutes');
+    secondsLabel = document.querySelector('.seconds');
+    totalSeconds = 0;
+    timerStarted = false;
+    movesLabel.innerHTML = moveCount;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+};
 
-var timer;
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
+initializeValues();
 
+var setupDeck = (function () {
+    //Create a clone of the card list (for pairing) and push into original array, creating the final deck array
+    const cardPairs = cardList;
+    deck = shuffle(cardList.concat(cardPairs));
+})();
+
+function resetGame() {
+    clearInterval(timer);
+    initializeValues();
+    deck = shuffle(deck);
+    deckContainer.innerHTML = '';
+    stars.getElementsByTagName('LI')[2].classList.remove('lost');
+    stars.getElementsByTagName('LI')[1].classList.remove('lost');
+    dealCards();
+}
 
 function setTime() {
     ++totalSeconds;
@@ -40,68 +74,26 @@ function setTime() {
     minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 }
 
+//Timer helper function
 function pad(val) {
-    var valString = val + "";
+    var valString = val + '';
     if (valString.length < 2) {
-        return "0" + valString;
+        return '0' + valString;
     } else {
         return valString;
     }
 }
 
-var timerStarted = false;
-var startTimer = (function() {
+function startTimer() {
     //Setup below is to enable single use only of this function
-    return function() {
-        if(!timerStarted) {
-            timerStarted = true;
-            timer = setInterval(setTime, 1000);
-        }
-    };
-})();
+    if(!timerStarted) {
+        timerStarted = true;
+        timer = setInterval(setTime, 1000);
+    }
+};
 
 function stopTimer() {
     clearInterval(timer);
-}
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-//Shuffle the deck
-deck = shuffle(deck);
-
-
-//Add click listener to the deck and flip the card if it's clicked
-const deckContainer = document.querySelector('.deck');
-deckContainer.addEventListener("click", flipCard);
-
-//Add click listener to the reset button
-const resetButton = document.querySelector('.reset');
-resetButton.addEventListener("click", resetGame);
-
-function resetGame() {
-    clearInterval(timer);
-    totalSeconds = 0;
-    secondsLabel.innerHTML = pad(totalSeconds % 60);
-    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-    moveCount = 0;
-    movesLabel.innerHTML = moveCount;
-    currentPairs = 0;
-    openCards = [];
-    deck = shuffle(deck);
-    clickDisabled = false;
-    timerStarted = false;
-    deckContainer.innerHTML = '';
-    stars.getElementsByTagName("LI")[2].classList.remove("lost");
-    stars.getElementsByTagName("LI")[1].classList.remove("lost");
-    starCount = 3;
-    starWinText = "stars";
-    dealCards();
 }
 
 //Loop through each card in deck and create its HTML
@@ -146,7 +138,7 @@ function flipCard(evt) {
         if (openCards.length === 0) {
             //No other cards are open so just show the card and add it to the open cards list
             //Show the card's face
-            card.classList.add("show", "open");
+            card.classList.add('show', 'open');
             //If there aren't any opened cards, add current card to the opened cards list
             openCards.push(card);
         } else {
@@ -161,7 +153,7 @@ function flipCard(evt) {
             //Disable clicking until cards are checked
             clickDisabled = true;
             //Show the card's face (moved here from flipCard() so it could be placed after the click disabler)
-            card.classList.add("show", "open");
+            card.classList.add('show', 'open');
             //Check if the opened cards match
             if (checkMatch(card.innerHTML, openCards[0].innerHTML)) {
                 successfulMatch(card, openCards[0]);
@@ -184,8 +176,8 @@ function checkMatch(firstCard, secondCard) {
 }
 
 function successfulMatch(firstCard, secondCard) {
-    firstCard.classList.add("match");
-    secondCard.classList.add("match");
+    firstCard.classList.add('match');
+    secondCard.classList.add('match');
     //Clear the open cards list
     openCards = [];
     //Increase the matched pair counter
@@ -198,12 +190,12 @@ function successfulMatch(firstCard, secondCard) {
 
 function unsuccessfulMatch(firstCard, secondCard) {
     //If they don't match, add a class indicating the mismatch to both cards
-    firstCard.classList.add("nomatch");
-    secondCard.classList.add("nomatch");
+    firstCard.classList.add('nomatch');
+    secondCard.classList.add('nomatch');
     //Wait 1 second, the reset the open cards and clear the opened cards list
     setTimeout(function () {
-        firstCard.classList.remove("show", "open", "nomatch");
-        secondCard.classList.remove("show", "open", "nomatch");
+        firstCard.classList.remove('show', 'open', 'nomatch');
+        secondCard.classList.remove('show', 'open', 'nomatch');
         openCards = [];
         //Allow clicking after the check has completed
         clickDisabled = false;
@@ -213,12 +205,12 @@ function unsuccessfulMatch(firstCard, secondCard) {
 function calculateRating() {
 
     if (moveCount === 11) {
-        stars.getElementsByTagName("LI")[2].classList.add("lost");
+        stars.getElementsByTagName('LI')[2].classList.add('lost');
         starCount = 2;
     } else if (moveCount === 21) {
-        stars.getElementsByTagName("LI")[1].classList.add("lost");
+        stars.getElementsByTagName('LI')[1].classList.add('lost');
         starCount = 1;
-        starWinText = "star";
+        starWinText = 'star';
     }
 }
 
